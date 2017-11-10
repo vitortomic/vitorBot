f056af8f36f9a5830bfac15d1a51ce3063509cdf
KC\vitor.tomic <vitor.tomic@vicert.com>
2017-11-10 11:08:50 +0100

    initial commit

---
+++ vitorBotara.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+
+const random = ()=>{
+    let x = Math.random();
+    return x >= 0.5 ? true : false;
+}
+
+const randomInt = ()=>{
+    return Math.floor(Math.random() * 10);
+}
+
+const izjava = ()=>{
+    return random() ? botara() : botara2();
+}
+
+const botara = ()=>{
+    return random() ? ayy() : "Xd";  
+}
+
+const botara2 = ()=>{
+    return random() ? "rofl" : "ha" + "ha".repeat(randomInt())
+}
+
+const ayy = ()=>{
+    return "ay" + "y".repeat(randomInt());
+}
+
+const randomVreme = ()=>{
+    return Math.floor(10000 * Math.random());
+}
+
+const ispis = ()=>{
+    let timeout = setTimeout(()=>{
+        let poruka = "Vitor Tomic: " + izjava() + "\t\t" + new Date().toLocaleString('en-US', { hour: 'numeric',minute:'numeric', hour12: true })
+        console.log(poruka);
+        writeLog(poruka).then((success)=>{
+            ispis();
+        }, (error)=>{
+            console.log('Error writing to log.txt');
+            clearTimeout(timeout);
+        })
+    }, randomVreme());
+}
+
+const initialize = async ()=>{
+    try{
+        let exists = await fileExistsPromise();
+        exists ? ispis() : await createFile();
+        ispis();
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+
+const fileExistsPromise = ()=>{
+    return new Promise((resolve)=>{
+        fs.exists('log.txt', (exists)=>{
+            resolve(exists);
+        });
+    })
+}
+
+const createFile = ()=>{
+    return new Promise((resolve, reject)=>{
+        fs.writeFile('log.txt', "", (error)=>{
+            error ? reject(error) : resolve(true);
+        });
+    })
+}
+
+const writeLog = (message)=>{
+    return new Promise((resolve,reject)=>{
+        fs.appendFile('log.txt', "\r\n" + message, (error)=>{
+            error ? reject(error) : resolve(true);
+        })
+    })
+}
+
+initialize();
